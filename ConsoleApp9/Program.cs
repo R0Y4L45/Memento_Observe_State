a0e6f4c6b6367fb8801876bffce5f80cfd2e1af2
R0Y4L45 <106491312+R0Y4L45@users.noreply.github.com>
2022-12-02 16:01:00 +0400

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,99 @@
+﻿public class Originator
+{
+
+    private String state;
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public String getState()
+    {
+        return state;
+    }
+
+    public Memento saveStateToMemento()
+    {
+        return new Memento(state);
+    }
+
+    public void getStateFromMemento(Memento Memento)
+    {
+        state = Memento.getState();
+    }
+}// End of the Originator class.  
+
+
+//This is a class.  
+
+public class Memento
+{
+
+    private String state;
+
+    public Memento(String state)
+    {
+        this.state=state;
+    }
+    public String getState()
+    {
+        return state;
+    }
+
+}// End of the 
+
+
+//This is a class.  
+
+
+
+public class Caretaker
+{
+
+    private List<Memento> mementoList = new List<Memento>();
+
+    public void Add(Memento state)
+    {
+        mementoList.Add(state);
+    }
+
+    public Memento Get(int index)
+    {
+        return mementoList[index];
+    }
+
+}// End of the Caretaker class.
+
+
+//This is a class.  
+
+
+public class MementoPatternDemo
+{
+    public static void Main()
+    {
+
+        Originator originator = new Originator();
+
+        Caretaker careTaker = new Caretaker();
+
+        originator.setState("State #1");
+        careTaker.Add(originator.saveStateToMemento());
+        originator.setState("State #2");
+        careTaker.Add(originator.saveStateToMemento());
+        originator.setState("State #3");
+        careTaker.Add(originator.saveStateToMemento());
+        originator.setState("State #4");
+
+        Console.WriteLine("Current State: " + originator.getState());
+        originator.getStateFromMemento(careTaker.Get(0));
+        Console.WriteLine("First saved State: " + originator.getState());
+        originator.getStateFromMemento(careTaker.Get(1));
+        Console.WriteLine("Second saved State: " + originator.getState());
+        originator.getStateFromMemento(careTaker.Get(2));
+        Console.WriteLine("Third saved State: " + originator.getState());
+    }
+
+}
+// End of the MementoPatternDemo class.  
---
+++ State/Program.cs
@@ -0,0 +1,181 @@
+﻿//This is an interface.  
+
+public interface Connection
+{
+
+    void Open();
+    void Close();
+    void Log();
+    void Update();
+}
+
+// End of the Connection interface. 
+
+
+//This is a class.  
+public class Accounting : Connection
+{
+
+    public void Open()
+    {
+        Console.WriteLine("open database for accounting");
+    }
+
+    public void Close()
+    {
+        Console.WriteLine("close the database");
+    }
+
+    public void Log()
+    {
+        Console.WriteLine("log activities");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Accounting has been updated");
+    }
+
+}// End of the Accounting class.
+
+
+//This is a class.  
+public class Sales : Connection
+{
+
+    public void Open()
+    {
+        Console.WriteLine("open database for sales");
+    }
+    public void Close()
+    {
+        Console.WriteLine("close the database");
+    }
+
+    public void Log()
+    {
+        Console.WriteLine("log activities");
+    }
+
+    public void Update()
+    {
+        Console.WriteLine("Sales has been updated");
+    }
+
+}// End of the Sales class.  
+
+
+
+//This is a class.  
+
+
+public class Management : Connection
+{
+       public void Open()
+{
+    Console.WriteLine("open database for Management");
+}
+       public void Close()
+{
+    Console.WriteLine("close the database");
+}
+
+       public void Log()
+{
+    Console.WriteLine("log activities");
+}
+
+       public void Update()
+{
+    Console.WriteLine("Management has been updated");
+}  
+  
+}  
+ // End of the Management class.  
+
+
+//This is a class.  
+  
+public class Controller
+{
+
+    public static Accounting? acct;
+    public static Sales? sales;
+    public static Management? management;
+
+    private static Connection? con;
+
+    Controller()
+    {
+        acct = new Accounting();
+        sales = new Sales();
+        management = new Management();
+    }
+
+    public void SetAccountingConnection()
+    {
+        con = acct;
+    }
+    public void SetSalesConnection()
+    {
+        con  = sales;
+    }
+    public void SetManagementConnection()
+    {
+        con  = management;
+    }
+    public void Open()
+    {
+        con.Open();
+    }
+    public void Close()
+    {
+        con.Close();
+    }
+    public void Log()
+    {
+        con.Log();
+    }
+    public void Update()
+    {
+        con.Update();
+    }
+
+
+}// End of the Controller class.  
+
+
+//This is a class.  
+
+
+public class StatePatternDemo
+{
+
+    public Controller controller;
+    StatePatternDemo(string con)
+    {
+        controller = new Controller();
+        //the following trigger should be made by the user  
+        if (con == "management")
+            controller.SetManagementConnection();
+        if (con == "sales")
+            controller.SetSalesConnection();
+        if (con == "accounting")
+            controller.SetAccountingConnection();
+        controller.Open();
+        controller.Log();
+        controller.Close();
+        controller.Update();
+    }
+
+
+    public static void Main(string args)
+    {
+
+        new StatePatternDemo(args[0].ToString());
+
+    }
+
+}
+
+// End of the StatePatternDemo class. 
